a14aed3f9bad4421c58bd4b809473a04e9877ab3
Dima Ian <ltr.tsts@mail.ru>
2020-03-03 12:08:21 +0300

    task# 5 - UserDAO refactored + '.idea' folder deleted.

--- src/main/java/util/TestMain.java
+++ src/main/java/util/TestMain.java
@@ -8,15 +8,11 @@ public class TestMain {
 
     public static void main(String[] args) {
 
-        System.out.println(HiberConnector.getSessionFactory());
-
-        UserHibernateDAO uhd = new UserHibernateDAO(HiberConnector.getSessionFactory());
-
-        System.out.println(uhd.getAllUsers());
-
-        User usr = new User();
-
-        System.out.println(UserDaoFactory.getConnectionDAO().getAllUsers());
+//        System.out.println(HiberConnector.getSessionFactory());
+//        UserHibernateDAO uhd = new UserHibernateDAO(HiberConnector.getSessionFactory());
+//        System.out.println(uhd.getAllUsers());
+//        User usr = new User();
+//        System.out.println(UserDaoFactory.getConnectionDAO().getAllUsers());
 
 
     }
